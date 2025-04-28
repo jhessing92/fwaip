10fef561235aa3758d1bd8fab1bb5ed9ed880f01
jhessing92 <jonathan.hessing@gmail.com>
2025-04-28 16:44:53 -0400

    Increased space between Features and wave divider

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -88,7 +88,7 @@ export default function Features() {
   return (
     <section 
       id="features" 
-      className="bg-white pt-12 md:pt-24 lg:pt-32 pb-20 md:pb-40 overflow-hidden relative"
+      className="bg-white pt-12 md:pt-24 lg:pt-32 pb-40 md:pb-56 lg:pb-64 overflow-hidden relative"
     >
       <div className="container mx-auto px-4 md:px-6">
         <motion.div
